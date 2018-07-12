841f615eafb762f5e2b83b8563b33e2bf7152372
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-12 10:03:47 -0700

    enabled secured login

--- server/user.js
+++ server/user.js
@@ -8,10 +8,23 @@ const User = models.getModel('user');
 
 // user list for debug
 Router.get('/list',function (req, res) {
+    // User.remove({},function(e,d){}); // remove all user data
     User.find({}, function(err,doc){
         return res.json(doc);
     });
 });
+
+Router.post('/login', function(req, res){
+    const {user, password} = req.body;
+    // findOne(search condition, display condition, callback) 0 means does not display.
+    User.findOne({user, password:md5Pwd(password)},{'password':0}, function(err, doc){
+        if(!doc){
+            return res.json({code:1, msg:'username does not exist or password is wrong.'});
+        }
+        return res.json({code:0, data: doc});
+    });
+});
+
 
 Router.post('/register',function(req, res){
     console.log(req.body);

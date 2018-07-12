42a0f0514514a88a1163a323b3bef03c538ccc5a
lincolnneu <hu.jiab@husky.neu.edu>
2018-07-12 08:22:56 -0700

    installed plugins: cookie parser and body parser.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
 const userRouter = require('./user');
 
 // create app
 const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json()); // to parse the json from post
 
 // start a middleware
 app.use('/user',userRouter);

0e14b1f0a8b282a6aea30c4b3b1e349e7c7ebf71
lincolnneu <hu.jiab@husky.neu.edu>
2018-08-05 23:36:38 -0700

    Enabled access controll allow origin from front end.

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const FRONTEND_URL = 'http://localhost:3000';
 
 
 const userRouter = require('./server/user');
 
 // create app
 const app = express();
+
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin",FRONTEND_URL); // we accept response from 4200
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS"); // accept these operations.
+    res.header("Access-Control-Allow-Credentials","true");
+    next(); // if successful, pass it through
+});
+
 
 app.use(cookieParser());
 app.use(bodyParser.json()); // to parse the json from post
